1f9171c58f006c6878dde07542e63817f1646144
Omar Trevino <omar.dintec@gmail.com>
2020-07-07 17:55:42 +0800

    added the picure url and displayed in the show page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,55 +1,53 @@
 class VenuesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:show, :index]
-    before_action :check_owner, only: [:edit]
+  skip_before_action :authenticate_user!, only: [:show, :index]
+  before_action :check_owner, only: [:edit]
 
-    def index
-      @venues = Venue.all
-    end
-
-
-    def show
-      @venue = Venue.find(params[:id])
-      # session[:stored_url] = request.url
-    end
-
-    def new
-      @venue = Venue.new
-    end
-
-    def create
-      @venue = Venue.new(venue_params)
-      @venue.user_id = current_user.id
-      @venue.save
-      # redirect_to venue_path(@venue)
-    end
-
-    def edit
-      @venue = Venue.find(params[:id])
-    end
-
-    def update
-      @venue = Venue.find(params[:id])
-      @venue.update(venue_params)
-      # redirect_to venue_path
-    end
-
-    def destroy
-      @venue = Venue.find(params[:id])
-      @venue.destroy
-      # redirect_to account_my_listings_path
-    end
-
-    private
-
-    def venue_params
-      params.require(:venue).permit(:name, :description, :address, :capacity, :venue_rating)
-    end
-
-    def check_owner
-      if current_user == Venue.find(params[:id]).user
-      else
-        redirect_to root_path, alert: "You got no permission to edit the page"
-      end
-    end
-
+  def index
+    @venues = Venue.all
   end
+
+  def show
+    @venue = Venue.find(params[:id])
+    # session[:stored_url] = request.url
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.user_id = current_user.id
+    @venue.save
+    # redirect_to venue_path(@venue)
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+    @venue.update(venue_params)
+    # redirect_to venue_path
+  end
+
+  def destroy
+    @venue = Venue.find(params[:id])
+    @venue.destroy
+    # redirect_to account_my_listings_path
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :description, :address, :capacity, :venue_rating)
+  end
+
+  def check_owner
+    if current_user == Venue.find(params[:id]).user
+    else
+      redirect_to root_path, alert: "You got no permission to edit the page"
+    end
+  end
+end
