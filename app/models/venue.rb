72b5dc3de062120ef560b28743ba924037ff9ff5
zijingkek <chrisartbp@gmail.com>
2020-07-07 18:20:32 +0800

    implement geocode

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,8 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
-  # after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_address,
