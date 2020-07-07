a4aa894a5be19edcde60d91dbcbe4333845c4105
zijingkek <chrisartbp@gmail.com>
2020-07-07 16:27:20 +0800

    basic search bar done

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,55 +1,67 @@
 class VenuesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:show, :index]
-    before_action :check_owner, only: [:edit]
+  skip_before_action :authenticate_user!, only: [:show, :index]
+  before_action :check_owner, only: [:edit]
 
-    def index
+  def index
+    @venues = Venue.all
+    if params[:search][:address].present?
+    # raise
+      @venues = Venue.search_by_name_and_address(params[:search][:address])
+    elsif params[:search][:address].blank?
       @venues = Venue.all
     end
 
+  end
 
-    def show
-      @venue = Venue.find(params[:id])
-      # session[:stored_url] = request.url
-    end
 
-    def new
-      @venue = Venue.new
-    end
-
-    def create
-      @venue = Venue.new(venue_params)
-      @venue.user_id = current_user.id
-      @venue.save
-      # redirect_to venue_path(@venue)
-    end
-
-    def edit
-      @venue = Venue.find(params[:id])
-    end
-
-    def update
-      @venue = Venue.find(params[:id])
-      @venue.update(venue_params)
-      # redirect_to venue_path
-    end
-
-    def destroy
-      @venue = Venue.find(params[:id])
-      @venue.destroy
-      # redirect_to account_my_listings_path
-    end
-
-    private
-
-    def venue_params
-      params.require(:venue).permit(:name, :description, :address, :capacity, :venue_rating)
-    end
-
-    def check_owner
-      if current_user == Venue.find(params[:id]).user
-      else
-        redirect_to root_path, alert: "You got no permission to edit the page"
-      end
-    end
+  def show
+    @venue = Venue.find(params[:id])
 
   end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.user_id = current_user.id
+    @venue.save
+    # redirect_to venue_path(@venue)
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+    @venue.update(venue_params)
+    # redirect_to venue_path
+  end
+
+  def destroy
+    @venue = Venue.find(params[:id])
+    @venue.destroy
+    # redirect_to account_my_listings_path
+  end
+
+  def search
+    @user_search = params[:search][:address]
+    @search_venues = Venue.search_by_name_and_address(@user_search)
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :description, :address, :capacity, :venue_rating)
+  end
+
+  def check_owner
+    if current_user == Venue.find(params[:id]).user
+    else
+      redirect_to root_path, alert: "You got no permission to edit the page"
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/s", to: "venues#search"
   resources :venues
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
