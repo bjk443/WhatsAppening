600efac74251c565c07cae4d5883ff40893e2abf
zijingkek <chrisartbp@gmail.com>
2020-07-08 19:23:34 +0800

    create and add chat to each channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :venues
+  resources :venues do
+    resources :chatrooms, only: :show
+    resources :messages, only: :create
+  end
 end
