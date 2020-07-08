a827423071327fafb2376fa1eff87128f75a1a2a
Omar Trevino <omar.dintec@gmail.com>
2020-07-08 19:29:25 +0800

    Changes in the event model and routes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,15 @@
 class Event < ApplicationRecord
-    has_many :ratings
-    belongs_to :venue
-    def average_rating
-        if self.ratings.count == 0
-            return 0
-        else
-            sum_rating = 0
-            self.ratings.each do |rating|
-            sum_rating += rating.stars
-            end
-        return sum_rating / self.ratings.count
-        end
+  has_many :ratings
+  belongs_to :venue
+  def average_rating
+    if self.ratings.count == 0
+      return 0
+    else
+      sum_rating = 0
+      self.ratings.each do |rating|
+        sum_rating += rating.stars
+      end
+      return sum_rating / self.ratings.count
     end
+  end
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_033206) do
+ActiveRecord::Schema.define(version: 2020_07_08_100605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_07_08_033206) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "venue_id", null: false
+    t.string "artist"
+    t.string "genre"
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
