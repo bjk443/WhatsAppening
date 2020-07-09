184cbdf519c002f06d2ee1f83a722c1dcd151f0d
Omar Trevino <omar.dintec@gmail.com>
2020-07-09 15:18:51 +0800

    Still fighting with APIIIII

--- app/javascript/channels/live_music_card.js
+++ app/javascript/channels/live_music_card.js
@@ -1,5 +1,6 @@
-fetch("https://api.deezer.com/playlist/908622995")
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+
+const initSoundCard = () => {
+  fetch("https://api.deezer.com/playlist/908622995",{
+    'mode': 'no-cors'
+  }).then(response => response.json()).then((data) => { console.log(data); });
+}
