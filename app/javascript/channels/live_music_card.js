896268d0a871c26ef1600254a19e94d269956a5d
Omar Trevino <omar.dintec@gmail.com>
2020-07-09 19:11:54 +0800

    Music plugin added

--- app/javascript/channels/live_music_card.js
+++ app/javascript/channels/live_music_card.js
@@ -1,11 +1,11 @@
 
-const initSoundCard = () => {
-  fetch("https://api.deezer.com/playlist/908622995",{
-    'mode': 'no-cors'
-  }).then(response => response.json()).then((data) => { console.log(data); });
-}
+// const initSoundCard = () => {
+//   fetch("https://api.deezer.com/playlist/908622995",{
+//     'mode': 'no-cors'
+//   }).then(response => response.json()).then((data) => { console.log(data); });
+// }
 
 
-fetch('https://api.deezer.com/playlist/908622995')
-  .then(response => response.json());
-  .then(data => console.log(data));
+// fetch('https://api.deezer.com/playlist/908622995')
+//   .then(response => response.json());
+//   .then(data => console.log(data));
