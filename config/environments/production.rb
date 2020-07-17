de07fb0bedfec544cf24d82efc1b847b82a7d3b2
zijingkek <chrisartbp@gmail.com>
2020-07-17 09:54:53 +0800

    added timezone Singapore to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "WhatsAppening_production"
+  config.time_zone = "Singapore"
+
 
   config.action_mailer.perform_caching = false
 
